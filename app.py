10a2659d79ffe74074ccae41a89dd9c73e8c3efc
shorya-singh4527 <shorya1308@gmail.com>
2025-09-25 21:31:19 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return """
+    <html>
+        <head><title>Hello World</title></head>
+        <body style="background-color: green; color: white; text-align: center; padding-top: 200px;">
+            <h1>Hello World</h1>
+        </body>
+    </html>
+    """
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=8080)
